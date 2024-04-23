164da7f4e73ceea40f57b7b80a4c81cbf9956c24
Carlos Vesta <vesta.carlos@gmail.com>
2024-04-23 22:56:01 +0200

    Adicionando a lesson-06

---
+++ lesson-06/js/script.js
@@ -0,0 +1,5 @@
+// Guardar um nome na variável
+var nome = window.prompt('Qual é o seu nome?');
+
+// Visualizar o nome guardado na variável
+window.alert('É um grande prazer te conhecer, ' + nome + '!');
---
+++ lesson-06/js/soma.js
@@ -0,0 +1,9 @@
+
+// Somatório de Números Inteiros
+var n1 = Number.parseInt(window.prompt('Digite o primeiro número: '));
+var n2 = Number.parseInt(window.prompt('Digite o segundo número: '));
+
+var soma = n1 + n2;
+
+window.alert('A soma de ' + n1 + ' e de ' + n2+ ' é igual a ' + soma );
+
