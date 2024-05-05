38123e020fdc0716ecbcdc861bcacfdaa9bc93e6
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-05 23:46:11 +0200

    07.operadores-aritmeticos(Soma)

---
+++ 07.operadores-aritmeticos/js/soma.js
@@ -0,0 +1,8 @@
+// Faça um programa que leia dois números e mostre o resultado da sua soma
+
+var numero1 = Number(prompt('Digite o primeiro número: '));
+var numero2 = Number(prompt('Digite o segundo número: '));
+
+var soma = numero1 + numero2;
+
+alert(`A soma de ${numero1} e ${numero2} é igual a ${soma}`);
