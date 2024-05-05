89c387a0046bca1ecdfc8b666bf6632676301fd2
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-05 23:51:54 +0200

    07.operadores-aritmeticos(Subtração)

---
+++ 07.operadores-aritmeticos/js/subtracao.js
@@ -0,0 +1,8 @@
+// Faça um programa que leia dois números e mostre o resultado da sua subtracao
+
+var numero1 = Number(prompt('Digite o primeiro número: '));
+var numero2 = Number(prompt('Digite o segundo número: '));
+
+var soma = numero1 - numero2;
+
+alert(`A subração de ${numero1} e ${numero2} é igual a ${soma}`);
