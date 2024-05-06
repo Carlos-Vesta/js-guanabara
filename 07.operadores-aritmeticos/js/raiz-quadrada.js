436616b80ac2f0f56608860c5b724c134ec142bd
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-06 22:22:41 +0200

    07.operadores-aritmeticos(Raiz Quadrada)

---
+++ 07.operadores-aritmeticos/js/raiz-quadrada.js
@@ -0,0 +1,7 @@
+// Faça um programa que leia um número e mostre a sua Raiz Quadrado.
+
+var numero = Number(prompt('Digite um número: '));
+
+var raizQuadrada = Math.sqrt(numero);
+
+alert(`A raiz quadrada de ${numero} é igual a ${raizQuadrada}`);
