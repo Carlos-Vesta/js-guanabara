17b71f02b81b7c92a5cd7bf8ff550619319ea771
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-06 22:16:24 +0200

    07.operadores-aritmeticos(Quadrado de um número)

---
+++ 07.operadores-aritmeticos/js/quadrado.js
@@ -0,0 +1,7 @@
+// Faça um programa que leia um número e mostre o Quadrado deste número.
+
+var numero = Number(prompt('Digite um número: '));
+
+var quadrado = numero ** 2;
+
+alert(`O quadrado de ${numero} é igual a ${quadrado}`);
