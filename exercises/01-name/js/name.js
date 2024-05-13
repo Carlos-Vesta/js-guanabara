088a37d00eb969e3205155c9eef59a0e488c93a6
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-13 22:11:45 +0200

    Exercise - 02.name-day-year

--- exercises/01-name/js/name.js
+++ exercises/01-name/js/name.js
@@ -1,4 +1,11 @@
+/*
 
+Criar um script que leia o nome de uma pessoa, e mostra uma mensagem de boas-vindas 
+de acordo com o valor digitado.
+
+Ex: Ola Carlos! Prazer em te conhecer.
+
+*/
 
 var nome = prompt('Digite seu nome: ')
 
---
+++ exercises/02.name-day-year/js/name-day-year.js
@@ -0,0 +1,17 @@
+
+/* 
+
+Criar um script que leia o dia, o mês e o ano de nascimento de uma pessoa, 
+e mostra uma mensagem com a data formatada. 
+
+Ex: Você nasceu no dia 4 de Dezembro de 1988. Correcto?
+
+*/
+
+var dia = String(prompt('Digite o dia do seu nascimento: ')) 
+var mes = String(prompt('Digite o mês do seu nascimento: '))
+var ano = String(prompt('Digite o ano do seu nascimento: '))
+
+alert(`Você nasceu no dia ${dia} de ${mes} de ${ano}. Correcto?`)
+
+
