a4264f650e9fe45ed13c9314e3a34d7a6507f168
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-13 22:34:06 +0200

    Exercise - 04.primitive-type

---
+++ exercises/04.primitive-type/js/primitive-type.js
@@ -0,0 +1,12 @@
+/*
+
+Faça um programa que leia algo pelo teclado e mostre o seu tipo primitivo e 
+todas as informações possíveis sobre ela.
+
+*/
+
+var valor = prompt('Digite algo pelo teclado: ')
+var tipoPrimitivo = typeof valor
+
+
+alert(`O valor ${valor} é do tipo ${tipoPrimitivo}`)
