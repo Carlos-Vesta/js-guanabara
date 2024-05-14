6ce25b76b7888c970b456ca6c0216ac050b6fa84
Carlos Vesta <vesta.carlos@gmail.com>
2024-05-14 20:38:54 +0200

    Exercise - 08.tabuada

---
+++ exercises/08.tabuada/js/tabuada.js
@@ -0,0 +1,34 @@
+/*
+
+Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+*/
+
+var numeroInteiro = Number.parseInt(prompt('Digite um número inteiro: '))
+
+var t1 = numeroInteiro * 1
+var t2 = numeroInteiro * 2
+var t3 = numeroInteiro * 3
+var t4 = numeroInteiro * 4
+var t5 = numeroInteiro * 5
+var t6 = numeroInteiro * 6
+var t7 = numeroInteiro * 7
+var t8 = numeroInteiro * 8
+var t9 = numeroInteiro * 9
+var t10 = numeroInteiro * 10
+var t11 = numeroInteiro * 11
+var t12 = numeroInteiro * 12
+
+alert(`A tabua de ${numeroInteiro} é:\n 
+${numeroInteiro} x 1 = ${t1}\n
+${numeroInteiro} x 2 = ${t2}\n
+${numeroInteiro} x 3 = ${t3}\n
+${numeroInteiro} x 4 = ${t4}\n
+${numeroInteiro} x 5 = ${t5}\n
+${numeroInteiro} x 6 = ${t6}\n
+${numeroInteiro} x 7 = ${t7}\n
+${numeroInteiro} x 8 = ${t8}\n
+${numeroInteiro} x 9 = ${t9}\n
+${numeroInteiro} x 10 = ${t10}\n
+${numeroInteiro} x 11 = ${t11}\n
+${numeroInteiro} x 12 = ${t12}`)
