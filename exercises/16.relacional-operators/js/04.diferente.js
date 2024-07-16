0c3f7981f8b646593efb83743c36432e3411c451
Carlos Vesta <vesta.carlos@gmail.com>
2024-07-16 05:20:12 +0200

    04.diferente

---
+++ exercises/16.relacional-operators/js/04.diferente.js
@@ -0,0 +1,9 @@
+
+/** 4. Verifique se dois números são diferentes. */
+
+var num1 = Number(prompt("Digite o primeiro numero: "))
+var num2 = Number(prompt("Digite o segudno numero: "))
+
+var diferente = num1 != num2
+
+alert(`O numero ${num1} != ${num2} = ${diferente}`)
