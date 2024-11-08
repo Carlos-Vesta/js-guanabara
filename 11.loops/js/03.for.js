c194fbb2490e8dc4b0542a86410d27c8768e160c
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-08 15:11:15 +0200

    03.for

---
+++ 11.loops/js/03.for.js
@@ -0,0 +1,14 @@
+/**
+ * Estrutura de Repetição com Variável de Controle
+ */
+
+var contador = 1
+
+console.log(`Vai começar a contar ...`)
+
+for (contador = 1; contador <= 5; contador++) {
+
+    console.log(contador)
+}
+
+console.log(`FIM!`)
