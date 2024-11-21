3fd34c9c20d3c1afb407f6a36b97950661d5a7e0
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-21 16:27:32 +0200

    03.array-comprimento

---
+++ 12.arrays/03.array-comprimento.js
@@ -0,0 +1,18 @@
+
+let num = [5, 7, 4, 12, 6, 8];
+
+// Irá colocar o 11 na posição 2, removendo o que já lá está
+num[2] = 11;
+
+// Irá colocar o 20 no final da lista
+num.push(20);
+
+// Irá mostrar o resultado dentro dos parenteses rectos (formatação padrão)
+// console.log(num);
+
+// Irá mostrar o resultado sem a formatação padrão
+console.log(`${num}`);
+
+// Irá mostrar o número de elementos no array
+console.log(`O comprimento do array tem ${num.length} elementos.`);
+
