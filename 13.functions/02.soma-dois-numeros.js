981bff401f3a029f99eeec4d9facca53e1008c28
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-23 08:27:57 +0200

    02.soma-dois-numeros

---
+++ 13.functions/02.soma-dois-numeros.js
@@ -0,0 +1,7 @@
+
+function soma(numero1, numero2) { // Os parâmetros podem ser atribuídos valores. Ex: numero1 = 0, numero2 = 0
+
+    return `A soma entre ${numero1} e ${numero2} é igual à ${(numero1 + numero2)}`;
+};
+
+console.log(soma(3, 6)); // Caso receba apenas um argumento, irá retornar NaN
