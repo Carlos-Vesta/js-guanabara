77513f47da2a2e66f97272e8be517a4085d199e6
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-22 18:26:51 +0200

    14.inversao-string.

---
+++ exercises/19.repeticoes/js/14.inversao-string.js
@@ -0,0 +1,40 @@
+/**
+ * 14 - Inversão de uma String (for)
+Escreva um programa que inverta uma string fornecida pelo usuário usando um loop `for`.
+ */
+
+
+// Criar o escutador
+document.querySelector(".input-button").addEventListener("click", inverterString);
+
+function inverterString() {
+
+    // Inicialização das variáveis
+    let userString = document.querySelector("#input-string").value.trim();
+    let viewResult = document.querySelector(".div-resultado");
+    let reversedString = "";
+
+
+
+    viewResult.innerHTML = ``; // Limpa o campo da mensagem
+
+
+    // Validação do formulário
+    if (userString.length === 0) {
+        viewResult.innerHTML = `Campo vazio, digite um texto!`;
+        viewResult.style.color = `#FF0000`;
+    }
+
+
+    // Inversão de String
+    for(let i = userString.length - 1; i >= 0; i--) {
+
+        reversedString += userString[i];
+    }
+
+    viewResult.style.color = `#000000`;
+    viewResult.style.textAlign = `justify`;
+    viewResult.innerHTML = `O texto do usuário é <strong>${userString}</strong>, e o texto
+     invertido é  <strong>${reversedString}</strong>`;
+
+}
