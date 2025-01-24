23c9b418196e31a93e3afd3b8f17f5ac0307d1e1
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-24 23:43:49 +0200

    15.contagem-vogais

--- exercises/19.repeticoes/js/15.contagem-vogais.js
+++ exercises/19.repeticoes/js/15.contagem-vogais.js
@@ -10,10 +10,11 @@ document.querySelector(".input-button").addEventListener("click", contarVogais);
 function contarVogais() {
 
     // Inicialização de variáveis
-    let vogais = document.querySelector("#input-string").value.trim();
+    let texto = document.querySelector("#input-string").value.trim();
     let viewResult = document.querySelector(".div-resultado");
-    let vagalList = ["a", "e", "i", "o", "u"];
-    let allVogais = "";
+
+    const contador = ["a", "e", "i", "o", "u"];
+    let vogais = 0;
 
 
     // Limpa o campo do texto
@@ -21,19 +22,19 @@ function contarVogais() {
 
 
     // Validação do formulário
-    if (vogais.length === 0) {
+    if (texto.length === 0) {
         viewResult.style.color = `#FF0000`;
         viewResult.innerHTML = `O campo não deve estar vazio`;
     }
 
 
     // Loop para encontrar as vogais
-    for (let i = vogais; i < vogais.legth; i--) {
-        if (vogais.legth === vagalList.length) {
-            allVogais += vogais[i]
-            viewResult.innerHTML += `${allVogais}`;
+    for (let i = 0; i < texto.legth; i++) {
+        if (vogais.includes(texto[i])) {
+            contador++;
         }
 
-        // viewResult.innerHTML += `${allVogais}`;
     }
+
+    viewResult.innerHTML = `${contador}`;
 }
