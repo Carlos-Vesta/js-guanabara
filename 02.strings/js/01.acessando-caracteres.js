320cfbd38f7051453a3ae905fcec989839c96adb
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-26 23:14:14 +0200

    01.acessando-caracteres

---
+++ 02.strings/js/01.acessando-caracteres.js
@@ -0,0 +1,21 @@
+/**
+ * Você pode acessar caracteres individuais em uma String usando colchetes ([])
+ * e o índice do caractere.
+ */
+
+const texto = "Olá Mundo!";
+
+
+console.log(texto[0]);
+console.log(texto[1]);
+console.log(texto[2]);
+console.log(texto[3]);
+console.log(texto[4]);
+console.log(texto[5]);
+console.log(texto[6]);
+console.log(texto[7]);
+console.log(texto[8]);
+console.log(texto[9]);
+console.log(texto[10]);
+
+// Espaços em branco são considerados caracteres
