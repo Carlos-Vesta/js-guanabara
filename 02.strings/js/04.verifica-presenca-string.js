dfecef51c7810c6c0cb3aa6fc0362c10e279f806
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-26 23:35:04 +0200

    04.verifica-presenca-string

---
+++ 02.strings/js/04.verifica-presenca-string.js
@@ -0,0 +1,14 @@
+/**
+ *Para verificar se uma substring está presente na string ou não, 
+ usamos a função "includes()"
+ */
+
+
+const texto = "Olá programação em Javascript";
+
+
+
+console.log(texto.includes("Olá")); // Retorna true
+console.log(texto.includes("code")); // Retorna false
+
+// Essas substrings são "case sensetive"
