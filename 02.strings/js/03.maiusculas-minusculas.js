a705fab61d5f4e30430341f3d907ec352782c88c
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-26 23:27:05 +0200

    03.maiusculas-minusculas

---
+++ 02.strings/js/03.maiusculas-minusculas.js
@@ -0,0 +1,13 @@
+/**
+ *Para converter uma String para maiúsculas ou minúsculas,
+ usamos as funções "toLowerCase()" para minúsculas e "toUpperCase()"
+ para maiúsculas.
+ */
+
+
+const texto = "Olá Javascript";
+
+
+console.log(texto.toLocaleLowerCase()); // olá javascript
+console.log(texto.toUpperCase()); // OLÁ JAVASCRIPT
+
