ce0d40f6d812b5465a8adab7a825964d6ca53827
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-26 23:20:32 +0200

    02.comprimento-string

---
+++ 02.strings/js/02.comprimento-string.js
@@ -0,0 +1,12 @@
+/**
+ *Para achar o comprimento de uma string usamos o "length", que conta quantos
+ caracteres tem em uma string.
+ */
+
+const texto = "Olá Mundo!";
+
+
+console.log(texto.length); // 10
+
+
+// Espaços em branco e pontos são considerados caracteres
