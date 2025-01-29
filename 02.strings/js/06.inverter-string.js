df3ac70446a4dd9b98d7a8d20c578b24264cd0c4
Carlos Vesta <vesta.carlos@gmail.com>
2025-01-29 23:33:12 +0200

    06.inverter-string-melhorado

--- 02.strings/js/06.inverter-string.js
+++ 02.strings/js/06.inverter-string.js
@@ -22,3 +22,25 @@ for (let i = texto.length - 1; i <= texto.length; i--) {
 }
 
 alert(invertedText); // Imprime o texto invertido
+
+
+/**
+ * CÓDIGO MELHORADO 1
+ * 
+ *  let texto = "Carlos";
+    let inverterTexto = "";
+
+    for (let i = texto.length - 1; i >= 0; i--) {
+        inverterTexto += texto.charAt(i);
+    }
+
+    console.log(inverterTexto);
+
+###########################################################
+
+    CÓDIGO MELHORADO 2
+
+    let texto = "Carlos";
+    let inverterTexto = texto.split("").reverse().join("");
+    console.log(inverterTexto);
+ */
